aab094a989f793a2b1a7d61dd068dfeccac55262
Serhii Haluzov <sehal@smart-it.com>
2021-11-27 11:27:46 +0200

    azure sql database connection established

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -1,4 +1,4 @@
-using System;
+/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -20,4 +20,52 @@ namespace WebApplication
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
-}
+}*/
+
+using System;
+using System.Data.SqlClient;
+class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            { 
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.ConnectionString="Server=tcp:banda-server-db.database.windows.net,1433;" +
+                                         "Initial Catalog=band_db;Persist Security Info=False;" +
+                                         "User ID=banda;Password=12345Sergey;MultipleActiveResultSets=False;" +
+                                         "Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                // builder.DataSource = "banda-server-db.database.windows.net"; 
+                // builder.UserID = "banda";            
+                // builder.Password = "12345Sergey";     
+                // builder.InitialCatalog = "band_db";
+         
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+                    
+                    connection.Open();       
+
+                    String sql = "SELECT name, collation_name FROM sys.databases";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                            }
+                        }
+                    }                    
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("\nDone. Press enter.");
+            Console.ReadLine(); 
+        }
+    }
