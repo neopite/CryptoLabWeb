19ceadc2d51581052d5f9c998782a1c6d70dec94
Serhii Haluzov <sehal@smart-it.com>
2021-11-27 13:18:26 +0200

    redeployed key vault

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -41,7 +41,7 @@ namespace WebApplication
                     Mode = RetryMode.Exponential
                 }
             };
-            var client = new SecretClient(new Uri("https://<your-unique-key-vault-name>.vault.azure.net/"), new DefaultAzureCredential(),options);
+            var client = new SecretClient(new Uri("https://band-vault.vault.azure.net/"), new DefaultAzureCredential(),options);
 
             KeyVaultSecret secret = client.GetSecret("db-password");
 
