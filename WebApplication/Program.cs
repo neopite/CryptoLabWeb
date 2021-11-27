50d2e49c6199a948bee8b85252b14d01e2d0a0ab
Serhii Haluzov <sehal@smart-it.com>
2021-11-27 12:46:32 +0200

    test default web page

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -1,7 +1,8 @@
-/*using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -16,56 +17,72 @@ namespace WebApplication
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        { 
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-    }
-}*/
-
-using System;
-using System.Data.SqlClient;
-class Program
-    {
-        static void Main(string[] args)
-        {
-            try 
-            { 
-                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-                builder.ConnectionString="Server=tcp:banda-server-db.database.windows.net,1433;" +
-                                         "Initial Catalog=band_db;Persist Security Info=False;" +
-                                         "User ID=banda;Password=12345Sergey;MultipleActiveResultSets=False;" +
-                                         "Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-                // builder.DataSource = "banda-server-db.database.windows.net"; 
-                // builder.UserID = "banda";            
-                // builder.Password = "12345Sergey";     
-                // builder.InitialCatalog = "band_db";
-         
-                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
-                {
-                    Console.WriteLine("\nQuery data example:");
-                    Console.WriteLine("=========================================\n");
-                    
-                    connection.Open();       
-
-                    String sql = "SELECT name, collation_name FROM sys.databases";
-
-                    using (SqlCommand command = new SqlCommand(sql, connection))
-                    {
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
-                            }
-                        }
-                    }                    
-                }
-            }
-            catch (SqlException e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-            Console.WriteLine("\nDone. Press enter.");
-            Console.ReadLine(); 
+            // return Host.CreateDefaultBuilder(args)
+            //     .ConfigureWebHostDefaults(webBuilder =>
+            //         webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+            //             {
+            //                 var settings = config.Build();
+            //
+            //                 config.AddAzureAppConfiguration(options =>
+            //                 {
+            //                     options.Connect(settings["ConnectionStrings:AppConfig"])
+            //                         .ConfigureKeyVault(kv =>
+            //                         {
+            //                             kv.SetCredential(new DefaultAzureCredential());
+            //                         });
+            //                 });
+            //             })
+            //             .UseStartup<Startup>());
         }
     }
+}
+
+// using System;
+// using System.Data.SqlClient;
+//
+// using Azure.Identity;
+// class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             try 
+//             { 
+//                 SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+//                 builder.ConnectionString="Server=tcp:banda-server-db.database.windows.net,1433;" +
+//                                          "Initial Catalog=band_db;Persist Security Info=False;" +
+//                                          "User ID=banda;Password=12345Sergey;MultipleActiveResultSets=False;" +
+//                                          "Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+//
+//                 using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+//                 {
+//                     Console.WriteLine("\nQuery data example:");
+//                     Console.WriteLine("=========================================\n");
+//                     
+//                     connection.Open();       
+//
+//                     String sql = "SELECT name, collation_name FROM sys.databases";
+//
+//                     using (SqlCommand command = new SqlCommand(sql, connection))
+//                     {
+//                         using (SqlDataReader reader = command.ExecuteReader())
+//                         {
+//                             while (reader.Read())
+//                             {
+//                                 Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+//                             }
+//                         }
+//                     }                    
+//                 }
+//             }
+//             catch (SqlException e)
+//             {
+//                 Console.WriteLine(e.ToString());
+//             }
+//             Console.WriteLine("\nDone. Press enter.");
+//             Console.ReadLine(); 
+//         }
+//     }
