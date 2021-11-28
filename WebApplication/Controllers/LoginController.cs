6ece59b57c9eb3beeb27fb02ec1bbd66a545610d
Yaroslav <Stamin4ik4@gmail.com>
2021-11-28 12:34:37 +0200

    add login(hash comparing from input login form and db)

--- WebApplication/Controllers/LoginController.cs
+++ WebApplication/Controllers/LoginController.cs
@@ -1,14 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using WebApplication.Model;
+using WebApplication.Model.DB;
+using WebApplication.Model.Hashing;
 
 namespace WebApplication.Controllers
 {
     [Route("/login")]
     public class LoginController : Controller
     {
-        public LoginController()
+        private ApplicationDbContext applicationDbContext;
+
+        public LoginController(ApplicationDbContext applicationDbContext)
         {
-            //TODO : add db as dependency
+            this.applicationDbContext = applicationDbContext;
         }
 
         [HttpGet]
@@ -20,9 +27,28 @@ namespace WebApplication.Controllers
         [HttpPost]
         public string Login(LoginInputForm loginInputForm)
         {
-            //TODO : 1.validate db hash + password hash from input
-            //       2.validate username before passwrod
-            return "Page";
+            Console.WriteLine("FEFEFREGREGRE");
+            var userFromDbByUsernameFromForm =
+                applicationDbContext.User.FirstOrDefault(user => string.Equals(loginInputForm.Username, user.Username));
+            if (userFromDbByUsernameFromForm == null)
+            {
+                return "Invalid username";
+            }
+
+            var saltForUsername =
+                applicationDbContext.PasswordSalt.FirstOrDefault(x => x.UserId == userFromDbByUsernameFromForm.Id);
+            var hashingAlgorithm = new SHA256PasswordHashProvider();
+            var hashedPassword = hashingAlgorithm.HashPasswordWithExistingSalt(loginInputForm.Password, saltForUsername.Salt).Hash;
+            Console.WriteLine("Password from input : " + loginInputForm.Password);
+            Console.WriteLine("SALT : " + saltForUsername.Salt);
+            Console.WriteLine("hash from input form : " + hashedPassword);
+            Console.WriteLine("From server : " + userFromDbByUsernameFromForm.Password);
+            if (string.Equals(userFromDbByUsernameFromForm.Password,
+                hashingAlgorithm.HashPasswordWithExistingSalt(loginInputForm.Password, saltForUsername.Salt).Hash))
+            {
+                return "YEPIII , IT`S CORRECT";
+            }
+            return "SMTH WRONG";
         }
     }
 }
--- WebApplication/Model/Hashing/IPasswordHashProvider.cs
+++ WebApplication/Model/Hashing/IPasswordHashProvider.cs
@@ -9,6 +9,7 @@ namespace WebApplication.Model.Hashing
     public interface IPasswordHashProvider
     {
         public SaltedPassword HashPasswordWithSalt(string password, int saltSize);
+        public SaltedPassword HashPasswordWithExistingSalt(string password, string salt);
     }
 
     public class SHA256PasswordHashProvider : IPasswordHashProvider
@@ -22,7 +23,18 @@ namespace WebApplication.Model.Hashing
             passwordWithSaltBytes.AddRange(passwordUtf8);
             passwordWithSaltBytes.AddRange(salt);
             byte[] digestBytes = SHA256.Create().ComputeHash(passwordWithSaltBytes.ToArray());
-            return new SaltedPassword(Convert.ToBase64String(salt), Convert.ToBase64String(digestBytes));
+            return new SaltedPassword(Convert.ToBase64String(digestBytes), Convert.ToBase64String(salt));
+        }
+
+        public SaltedPassword HashPasswordWithExistingSalt(string password, string salt)
+        {
+            var passwordUtf8 = Encoding.UTF8.GetBytes(password);
+            var saltBytes = Convert.FromBase64String(salt);
+            List<byte> passwordWithSaltBytes = new List<byte>();
+            passwordWithSaltBytes.AddRange(passwordUtf8);
+            passwordWithSaltBytes.AddRange(saltBytes);
+            byte[] digestBytes = SHA256.Create().ComputeHash(passwordWithSaltBytes.ToArray());
+            return new SaltedPassword(Convert.ToBase64String(digestBytes), salt);
         }
     }
 }
