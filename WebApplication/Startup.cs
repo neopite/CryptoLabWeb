4870111c16d5fa7c9424ac404773c2abb87219f5
Serhii Haluzov <sehal@smart-it.com>
2021-11-28 13:44:09 +0200

    smth

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -40,19 +40,20 @@ namespace WebApplication
 
             app.UseRouting();
             
-            // SecretClientOptions options = new SecretClientOptions()
-            // {
-            //     Retry =
-            //     {
-            //         Delay= TimeSpan.FromSeconds(2),
-            //         MaxDelay = TimeSpan.FromSeconds(16),
-            //         MaxRetries = 5,
-            //         Mode = RetryMode.Exponential
-            //     }
-            // };
-            // var client = new SecretClient(new Uri("https://band-vault.vault.azure.net/"), new DefaultAzureCredential(),options);
-            //
-            // KeyVaultSecret secret = client.GetSecret("db-password");
+            SecretClientOptions options = new SecretClientOptions()
+            {
+                Retry =
+                {
+                    Delay= TimeSpan.FromSeconds(2),
+                    MaxDelay = TimeSpan.FromSeconds(16),
+                    MaxRetries = 5,
+                    Mode = RetryMode.Exponential
+                }
+            };
+            var client = new SecretClient(new Uri("https://band-vault.vault.azure.net/"), new DefaultAzureCredential(),options);
+            
+            KeyVaultSecret secret = client.GetSecret("db-password");
+            Console.WriteLine(secret);
 
             app.UseEndpoints(endpoints =>
             {
