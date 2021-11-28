b9621b70579593f5e6a49a38ca5c60464ce390ba
Yaroslav <Stamin4ik4@gmail.com>
2021-11-28 16:51:59 +0200

    add AES encryption/decryption

---
+++ WebApplication/Model/AES/DataCypherSolver.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace WebApplication.Model.AES
+{
+    public interface IDataCypherSolver
+    {
+        public byte[] EncryptPassword(string plainText, byte[] Key, byte[] IV);
+        public string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV);
+
+
+    }
+
+    public class DataCypherSolver : IDataCypherSolver
+    {
+        public byte[] EncryptPassword(string plainText, byte[] Key, byte[] IV)
+        {
+            byte[] encrypted;
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+                var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+                using (var msEncrypt = new MemoryStream())
+                {
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+            return encrypted;
+        }
+        
+        public string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            string plaintext;
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+                var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (var msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return plaintext;
+        }
+    }
+    
+}
