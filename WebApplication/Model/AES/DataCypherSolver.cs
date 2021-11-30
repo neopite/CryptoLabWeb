3649fc5e6c14d26e9e51bd3e69d9a3aa3fb7508d
Yaroslav <Stamin4ik4@gmail.com>
2021-11-30 23:28:38 +0200

    ebuchaa kodirovka

--- WebApplication/Model/AES/DataCypherSolver.cs
+++ WebApplication/Model/AES/DataCypherSolver.cs
@@ -36,7 +36,7 @@ namespace WebApplication.Model.AES
                     }
                 }
             }   
-            return (Encoding.UTF32.GetString(encrypted));
+            return (Encoding.UTF8.GetString(encrypted));
         }
         
         public string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
